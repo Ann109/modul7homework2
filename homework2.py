31f67f5325ffd5be9f27a488898aa35d27a9cf7b
Ann109 <andersonhloa@gmail.com>
2024-11-07 15:59:47 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,7 +1,7 @@
 def custom_write(file_name: str, strings: list):
         strings_positions = {}
         line_number = 1
-        file = open(file_name, 'w')
+        with open(file_name, 'w', encoding='utf-8') as file:
         for string in strings:
                 line_byte = file.tell()
                 file.write(f'{string}\n')
